ef1a87276a072fa59d8f9a518053665cb7eecf25
Salvatore Laisa <moebiusmania@gmail.com>
2022-05-07 15:41:26 +0200

    feat: add PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/subs-tracker/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "Subscriptions Tracker",
+        short_name: "Subscriptions Tracker",
+        description:
+          "Webapp to keep track on subscriptions fees and have some math done for you.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/favicon.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "/favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+          {
+            src: "/favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
