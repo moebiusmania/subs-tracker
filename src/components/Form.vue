debce8d98621dd774280f86326fbfb1c34a0526b
moebiusmania <moebiusmania@gmail.com>
2023-02-05 20:07:51 +0100

    chore: project maintenance

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -39,68 +39,30 @@ const onSubmit = (event: Event): void => {
 <template>
   <form @submit="onSubmit">
     <label for="name" class="label">Service name:</label>
-    <input
-      type="text"
-      placeholder="ex: Netflix, Prime, etc.."
-      class="input w-full input-bordered"
-      name="name"
-      v-model="data.name"
-      required
-    />
+    <input type="text" placeholder="ex: Netflix, Prime, etc.." class="input w-full input-bordered" name="name"
+      v-model="data.name" required />
 
     <label for="price" class="label">How much does it costs?</label>
-    <input
-      type="number"
-      placeholder="10.00"
-      class="input w-full input-bordered"
-      min="0.1"
-      step="0.1"
-      :value="data.price.toString()"
-      @input="updatePrice"
-      name="price"
-      required
-    />
+    <input type="number" placeholder="10.00" class="input w-full input-bordered" min="0.1" step="0.1"
+      :value="data.price.toString()" @input="updatePrice" name="price" required />
 
     <label for="currency" class="label">Currency:</label>
-    <select
-      class="select select-bordered w-full"
-      required
-      name="currency"
-      v-model="data.currency"
-      disabled
-    >
+    <select class="select select-bordered w-full" required name="currency" v-model="data.currency" disabled>
       <option value="€">€ Euro</option>
       <option value="$">$ Dollar</option>
     </select>
 
     <label class="label cursor-pointer" for="isactive">
       <span>Is active?</span>
-      <input
-        type="checkbox"
-        class="toggle"
-        name="isactive"
-        checked
-        v-model="data.isActive"
-      />
+      <input type="checkbox" class="toggle" name="isactive" v-model="data.isActive" />
     </label>
 
     <label for="expiration" class="label">Date of expiration:</label>
-    <input
-      type="date"
-      class="input w-full input-bordered"
-      required
-      name="expiration"
-      :value="formatDate(data.expiration)"
-      @input="updateExpiration"
-    />
+    <input type="date" class="input w-full input-bordered" required name="expiration"
+      :value="formatDate(data.expiration)" @input="updateExpiration" />
 
     <label for="recurrence" class="label">Recurs:</label>
-    <select
-      class="select select-bordered w-full"
-      required
-      name="recurrence"
-      v-model="data.recurrence"
-    >
+    <select class="select select-bordered w-full" required name="recurrence" v-model="data.recurrence">
       <option value="monthly">Monthly</option>
       <option value="yearly">Yearly</option>
     </select>
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import vue from "@vitejs/plugin-vue";
 
 const id: string = "/subs-tracker/";
 
+const isPROD: boolean = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: id,
+  server: {
+    port: 3000,
+  },
+  base: isPROD ? id : "/",
   plugins: [vue()],
 });
